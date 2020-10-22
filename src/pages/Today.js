c841868eb8c7af6822e822e0270d88820d13a69f
leegakyeong <alaska7132@naver.com>
2020-10-22 21:02:45 +0900

    Add page description and save button

--- src/pages/Today.js
+++ src/pages/Today.js
@@ -2,9 +2,14 @@ import React from 'react';
 import EmojiButton from '../components/EmojiButton';
 
 function Today() {
+  function handleClick() {
+    console.log('clicked');
+  }
+
   return (
     <div>
       <header>Today</header>
+      <div>오늘 안 먹은 것을 고르세요?</div>
       <main>
         <EmojiButton food="🐖" />
         <EmojiButton food="🐄" />
@@ -12,6 +17,7 @@ function Today() {
         <EmojiButton food="🥚" />
         <EmojiButton food="🥛" />
       </main>
+      <button onClick={handleClick}>저장하기</button>
     </div>
   );
 }
