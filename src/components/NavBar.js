e6946fae483a58ec0d40843c5c88ecc669ae9d0f
hjbc0921 <hjbc0921@gmail.com>
2020-10-25 20:32:55 +0900

    update main page layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
 
   return (
       <Layout>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{position:"fixed",left:0,bottom:0,width:"100%"}}>
           <Menu.Item key="1" style={{width:"25%",textAlign:"center"}}><Link to="/">Home</Link></Menu.Item>
           <Menu.Item key="2" style={{width:"25%",textAlign:"center"}}><Link to={`/today/${today.format('YYYY-MM-DD')}`}>Today</Link></Menu.Item>
           <Menu.Item key="3" style={{width:"25%",textAlign:"center"}}><Link to="/badge">Badge</Link></Menu.Item>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Calendar from '../components/Calendar';
 import NavBar from "../components/NavBar";
+import {Layout} from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function Main() {
   return (
-    <div>
-      <header>Main</header>
-      <Calendar />
-      <NavBar/>
-    </div>
+    <Layout>
+        <Content style={{padding:'10px 50px',minHeight:"100vh",backgroundColor:"white"}}>
+            <div className="site-layout-content">
+                <Calendar/>
+            </div>
+        </Content>
+        <NavBar/>
+    </Layout>
   );
 }
 
