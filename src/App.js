4f6d929eafa77bc0a33f46593c781009addd8e0d
hjbc0921 <hjbc0921@gmail.com>
2020-10-25 21:05:33 +0900

    update Today page with correct dates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import {Layout} from "antd";
 import Main from './pages/Main';
--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import EmojiButton from '../components/EmojiButton';
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import {Button, Layout} from "antd";
 
-function Today() {
-  return (
-    <div>
-      <header>Today</header>
-      <main>
-        <EmojiButton />
-        <EmojiButton />
-        <EmojiButton />
-      </main>
-    </div>
-  );
+const { Content } = Layout;
+
+class Today extends React.PureComponent {
+    static propTypes = {
+        match: PropTypes.object.isRequired,
+        location: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired
+    }
+    render () {
+        const { match} = this.props
+
+        return (
+            <Layout>
+                <Content style={{padding:'40px 20px',minHeight:"100vh",backgroundColor:"white"}}>
+                    <div className="site-layout-content">
+                        <div className="site-calendar-demo-card" style={{textAlign:"center",marginTop:"20px"}}>
+                            <Button style={{float:"right"}}><Link to="/">Home</Link></Button>
+                        <h2>{match.params.dayId}</h2>
+                        <main>
+                            <EmojiButton/>
+                            <EmojiButton/>
+                            <EmojiButton/>
+                        </main>
+                        </div>
+                    </div>
+                </Content>
+            </Layout>
+        )
+    }
 }
+
 
 export default Today;
