b3685376f5edec5504f23c4186cbbe4b816d9664
hjbc0921 <hjbc0921@gmail.com>
2020-10-25 19:28:02 +0900

    add bottom tab component NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {Link} from "react-router-dom";
+import {Layout, Menu} from "antd";
+import moment from "moment";
+
+function NavBar() {
+  let today = moment();
+
+  return (
+      <Layout>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" style={{width:"25%",textAlign:"center"}}><Link to="/">Home</Link></Menu.Item>
+          <Menu.Item key="2" style={{width:"25%",textAlign:"center"}}><Link to={`/today/${today.format('YYYY-MM-DD')}`}>Today</Link></Menu.Item>
+          <Menu.Item key="3" style={{width:"25%",textAlign:"center"}}><Link to="/badge">Badge</Link></Menu.Item>
+          <Menu.Item key="4" style={{width:"25%",textAlign:"center"}}><Link to="/setting">Setting</Link></Menu.Item>
+        </Menu>
+      </Layout>
+  );
+}
+
+export default NavBar;
