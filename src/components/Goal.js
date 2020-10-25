6928a9554fedf3fffd97642fbe6a885918c118ab
hjbc0921 <hjbc0921@gmail.com>
2020-10-25 21:18:39 +0900

    update Goal style

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Progress, Card } from 'antd';
 
 const Goal = () => {
   return (
       <div className="main-card">
-          <Card>목표 100% 달성</Card>
+          <Card>
+              <Card.Grid style={{width:"60%",textAlign:"center",marginTop:"20px"}}>이번 달 목표 달성률</Card.Grid>
+              <Card.Grid style={{width:"40%",textAlign:"center"}}><Progress type="circle" percent={75} width={60} /></Card.Grid>
+          </Card>
       </div>
   );
 }
