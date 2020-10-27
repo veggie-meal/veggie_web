d9d56a24186ed75a9a8827d5efe47b8b17ff8727
KIM HYO RIN <wwhurin0515@gmail.com>
2020-10-27 23:43:59 +0900

    add kakao login page, logig api(axios)

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ import {
 } from "react-router-dom";
 import Main from './pages/Main';
 import Today from './pages/Today';
+import Login from './pages/Login'
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/main">
             <Main />
           </Route>
           <Route exact path="/today">
             <Today />
           </Route>
+          <Route exact path="/">
+            <Login />
+          </Route>
         </Switch>
-        <nav>
+        {/* <nav>
           <Link to="/">Main</Link>
           <Link to="/today">Today</Link>
-        </nav>
+        </nav> */}
       </Router>
     </div>
   );
