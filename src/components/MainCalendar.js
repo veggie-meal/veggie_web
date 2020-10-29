dcba32f72142c1899543e0546daeaf5bc1cfff07
leegakyeong <alaska7132@naver.com>
2020-10-29 23:33:02 +0900

    Add server connection to MainCalendar

--- src/components/MainCalendar.js
+++ src/components/MainCalendar.js
@@ -2,31 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { Calendar, Avatar } from 'antd';
 import { withRouter} from "react-router-dom";
 import moment from 'moment';
+import axios from 'axios';
+
+import * as config from '../config';
+import * as colors from '../colors';
 
 const listData = {};
-listData['2020-10-05'] = 'vegan';
-listData['2020-10-06'] = 'vegan';
-listData['2020-10-07'] = 'vegan';
-listData['2020-10-08'] = 'vegan';
-listData['2020-10-15'] = 'ovo';
-listData['2020-10-25'] = 'ovo';
-listData['2020-10-09'] = 'ovo';
+listData['2020-10-05'] = 'VEGAN';
+listData['2020-10-06'] = 'POLLO';
+listData['2020-10-07'] = 'FLEXITARIAN';
+listData['2020-10-08'] = 'VEGAN';
+listData['2020-10-15'] = 'LACTO';
+listData['2020-10-25'] = 'FLEXITARIAN';
+listData['2020-10-09'] = 'PESCO';
 
-function dateFullCellRender(value) {
+function dateFullCellRender(dateValue) {
   let color = '#faf3dd';
-  switch (listData[value.format('YYYY-MM-DD')]) {
-    case ('vegan'):
-      color = '#68b0ab';
+  switch (listData[dateValue.format('YYYY-MM-DD')]) {
+    case 'VEGAN':
+      color = colors.GREEN;
       break;
-    case ('ovo'):
-      color = '#c8d5b9';
+    case 'LACTO':
+      color = colors.LIGHT_GREEN_1;
       break;
+    case 'OVO':
+    case 'LACTO-OVO':
+    case 'PESCO':
+    case 'POLLO':
+    case 'FLEXITARIAN':
     default:
         break;
   }
     return (
       <Avatar style={{ textDecorationStyle: 'bold', color: 'black', backgroundColor: color, verticalAlign: 'middle' }} size="medium">
-        {value.date()}
+        {dateValue.date()}
       </Avatar>
     );
 }
@@ -35,7 +44,18 @@ function MainCalendar(props) {
   const [value, setValue] = useState(moment());
 
   useEffect(() => {
-
+    const url = config.API_ADDR + 'api/calander/list';
+    axios.get(url, {
+      startDate: 1,
+      endDate: 31,
+      userId: 1,
+    })
+    .then(function(res) {
+      // res.calanderList
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
   });
 
   function onSelect(selected) {
