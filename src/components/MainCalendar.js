d3b437e6dffd1898690d194e883dcea17e3052b4
leegakyeong <alaska7132@naver.com>
2020-10-29 22:57:19 +0900

    Refactor MainCalendar

--- src/components/MainCalendar.js
+++ src/components/MainCalendar.js
@@ -1,72 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, Avatar } from 'antd';
 import { withRouter} from "react-router-dom";
 import moment from 'moment';
 
 const listData = {};
-listData['2020-03-05'] = 'vegan';
-listData['2020-03-06'] = 'vegan';
-listData['2020-03-07'] = 'vegan';
-listData['2020-03-08'] = 'vegan';
-listData['2020-03-15'] = 'ovo';
-listData['2020-03-25'] = 'ovo';
-listData['2020-03-09'] = 'ovo';
+listData['2020-10-05'] = 'vegan';
+listData['2020-10-06'] = 'vegan';
+listData['2020-10-07'] = 'vegan';
+listData['2020-10-08'] = 'vegan';
+listData['2020-10-15'] = 'ovo';
+listData['2020-10-25'] = 'ovo';
+listData['2020-10-09'] = 'ovo';
 
 function dateFullCellRender(value) {
-    var color = '#faf3dd';
-    console.log(listData[value.format('YYYY-MM-DD')])
-    switch (listData[value.format('YYYY-MM-DD')]) {
-        case ('vegan'):
-            color = '#68b0ab';
-            break;
-        case ('ovo'):
-            color = '#c8d5b9';
-            break;
-        default:
-            break;
-
-    }
+  let color = '#faf3dd';
+  switch (listData[value.format('YYYY-MM-DD')]) {
+    case ('vegan'):
+      color = '#68b0ab';
+      break;
+    case ('ovo'):
+      color = '#c8d5b9';
+      break;
+    default:
+        break;
+  }
     return (
-        <Avatar style={{ textDecorationStyle: 'bold', color: 'black', backgroundColor: color, verticalAlign: 'middle' }} size="medium">
-            {value.date()}
-        </Avatar>
+      <Avatar style={{ textDecorationStyle: 'bold', color: 'black', backgroundColor: color, verticalAlign: 'middle' }} size="medium">
+        {value.date()}
+      </Avatar>
     );
 }
 
-class MainCalendar extends React.Component {
-    state = {
-        value: moment('2020-03-25'),
-        selectedValue: moment('2020-03-25'),
-    };
+function MainCalendar(props) {
+  const [value, setValue] = useState(moment());
 
-    onSelect = value => {
-        if (this.state.value.format('YYYY-MM') === value.format('YYYY-MM')) {
-            this.props.history.push(`/today/${value.format('YYYY-MM-DD')}`)
-        }
-        this.setState({
-            value,
-            selectedValue: value,
-        });
-    };
+  useEffect(() => {
 
-    onPanelChange = (value, mode) => {
-        console.log(mode);
+  });
+
+  function onSelect(selected) {
+    if (value.format('YYYY-MM') === selected.format('YYYY-MM')) {
+      props.history.push(`/today/${selected.format('YYYY-MM-DD')}`) // 이 부분 바꿔야 함
     }
+    setValue(value);
+  };
 
-    render() {
-        const { value } = this.state;
-        return (
-            <div className="site-calendar-demo-card">
-                <Calendar
-                    dateFullCellRender={dateFullCellRender}
-                    fullscreen={false}
-                    value={value}
-                    onSelect={this.onSelect}
-                    onPanelChange={this.onPanelChange}
-                />
-            </div>
-        );
-    }
+  return (
+    <div className="site-calendar-demo-card">
+      <Calendar
+        dateFullCellRender={dateFullCellRender}
+        fullscreen={false}
+        value={value}
+        onSelect={onSelect}
+      />
+    </div>
+  );
 }
 
 export default withRouter(MainCalendar);
