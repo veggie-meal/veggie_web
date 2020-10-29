cae0ef4dc6ead521a6e1a284eefb216806e0a9c7
leegakyeong <alaska7132@naver.com>
2020-10-29 20:04:44 +0900

    Adjust EmojiButton styling

--- src/components/EmojiButton.js
+++ src/components/EmojiButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import './EmojiButton.css';
 
 function EmojiButton({ food }) {
   return (
-    <div>
-      <input type="checkbox" id={food} className="emoji-button-input" />
-      <label htmlFor={food} className="emoji-button-label">{ food }</label>
+    <div className="emojibutton-container">
+      <input type="checkbox" id={food} className="emojibutton-input" />
+      <label htmlFor={food} className="emojibutton-label">{ food }</label>
     </div>
   );
 }
