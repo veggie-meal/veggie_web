50f45283ce8079b85a21cc858b5a4e6bf7a187cf
leegakyeong <alaska7132@naver.com>
2020-10-30 00:11:46 +0900

    Add axios code to badge page

--- src/components/BadgeItem.js
+++ src/components/BadgeItem.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Image } from 'antd';
 
-const badges = ["/image/sky.png", "/image/apple.png", "/image/eggplant.png", "/image/cactus.png",
-  "/image/tree.png", "/image/vegetable.png", "/image/carrot.png"]
+const badgeImages = {
+  1: '/image/eggplant.png',
+  2: '/image/apple.png',
+  3: '/image/cactus.png',
+  4: '/image/veggie.png',
+  5: '/image/vegetable.png',
+  6: '/image/underthesea.png',
+  7: '/image/sky.png',
+  8: '/image/tree.png',
+  9: '/image/carrot.png',
+};
 
-const BadgeItem = (id) => {
-  console.log(id.id);
+const BadgeItem = ({ id }) => {
   return (
     <div>
       <Image
         width="100%"
-        src={badges[id.id]}
+        src={badgeImages[id]}
       />
     </div>
   );
--- src/pages/Badge.js
+++ src/pages/Badge.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row } from 'antd';
+import axios from 'axios';
+
 import BadgeItem from '../components/BadgeItem';
+import * as config from '../config';
 
 function Badge() {
+  const [badges, setBadges] = useState([]);
+
+  useEffect(() => {
+    axios.get(config.API_ADDR + 'veggie/list', { userId: 1 })
+    .then(function(res) {
+      setBadges(res.veggieList.filter(veggie => veggie.is_my === 1));
+    })
+    .catch(function(err) {
+      console.log(err);
+      setBadges([
+        {
+          "veggie_id": 1,
+          "veggie_name": "가지",
+          "is_my": 1,
+          "wrt_time": "2020-10-21T15:00:00.000Z"
+        },
+        {
+          "veggie_id": 2,
+          "veggie_name": "사과",
+          "is_my": 1,
+          "wrt_time": "2020-10-22T15:00:00.000Z"
+        },
+        {
+          "veggie_id": 3,
+          "veggie_name": "선인장",
+          "is_my": 1,
+          "wrt_time": "2020-10-24T15:00:00.000Z"
+        },
+        {
+          "veggie_id": 4,
+          "veggie_name": "베지",
+          "is_my": 1,
+          "wrt_time": null
+        },
+        {
+          "veggie_id": 5,
+          "veggie_name": "베지얼굴",
+          "is_my": 1,
+          "wrt_time": null
+        },
+        {
+          "veggie_id": 6,
+          "veggie_name": "바다",
+          "is_my": 1,
+          "wrt_time": null
+        },
+        {
+          "veggie_id": 7,
+          "veggie_name": "구름",
+          "is_my": 1,
+          "wrt_time": null
+        },
+        {
+          "veggie_id": 8,
+          "veggie_name": "나무",
+          "is_my": 1,
+          "wrt_time": null
+        },
+        {
+          "veggie_id": 9,
+          "veggie_name": "당근",
+          "is_my": 1,
+          "wrt_time": null
+        }
+      ]);
+    });
+  }, []);
+
   let badgeNum = 7;
   let colCount = 3;
   let rowCount = badgeNum / colCount;
@@ -22,9 +93,12 @@ function Badge() {
     )
   }
 
+  let badgeList = [];
+  badges.forEach(badge => badgeList.push(<BadgeItem id={badge.veggie_id} />));
+
   return (
-    <div className="site-layout-content">
-      {rows}
+    <div className="site-layout-content" style={{display:'grid', gridTemplateRows:'repeat(3, 1fr)', gridTemplateColumns:'repeat(3, 1fr)'}}>
+      {badgeList}
     </div>
   );
 }
