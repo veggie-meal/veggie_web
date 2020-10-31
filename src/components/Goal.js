93e1195a31802d06c5c368b6d5eece15e0aea76c
leegakyeong <alaska7132@naver.com>
2020-10-31 13:45:24 +0900

    Update percentage appearance

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Progress, Card } from 'antd';
 
 function Goal({ percentage }) {
+  const newPercentage = percentage.toFixed(2);
   return (
     <div className="main-card">
       <Card>
         <Card.Grid style={{width:"60%",textAlign:"center",marginTop:"20px"}}>이번 달 목표 달성률</Card.Grid>
         <Card.Grid style={{width:"40%",textAlign:"center"}}>
-          <Progress type="circle" percent={percentage} width={60} strokeColor="#68b0ab" />
+          <Progress type="circle" percent={newPercentage} width={60} strokeColor="#68b0ab" />
         </Card.Grid>
       </Card>
     </div>
